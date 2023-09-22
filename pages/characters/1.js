d94aa617d88575b6d9e7367bb38ba817eaf6ee81
Aleksandra <abaibik@gmail.com>
2023-09-22 12:32:48 +0200

    fetch 1 person

--- pages/characters/1.js
+++ pages/characters/1.js
@@ -1,17 +1,51 @@
+import { useState } from "react";
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
+import useSWR from "swr";
+
+const URL = "https://swapi.dev/api/people/";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
 
 export default function Character() {
-  const id = 1;
+  const [id, setId] = useState(1);
+
+  const { data, error, isLoading } = useSWR(URL + id, fetcher);
+
+  if (error) {
+    return <Layout>{error}</Layout>;
+  }
+
+  if (isLoading) {
+    return (
+      <Layout>
+        <div>Loading...</div>
+      </Layout>
+    );
+  }
+
+  const { name, height, eye_color, birth_year } = data;
 
   return (
     <Layout>
       <Card
         id={id}
-        name={"Luke Skywalker"}
-        height={172}
-        eyeColor={"blue"}
-        birthYear={"19BBY"}
+        name={name}
+        height={height}
+        eyeColor={eye_color}
+        birthYear={birth_year}
       />
     </Layout>
   );
